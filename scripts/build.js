3a53360c8e6b208387a00a938bd9d10092e5b4dc
amclio <lincroe@protonmail.com>
2022-11-06 19:44:56 +0900

    Fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,9 @@ const fs = require('fs')
 
 const path = 'libs/config.js'
 
-fs.unlinkSync(path)
+try {
+  fs.unlinkSync(path)
+} catch (err) {}
 
 fs.appendFileSync(path, `export const baseUrl = "${process.env.BASE_URL}"\n`)
 fs.appendFileSync(
